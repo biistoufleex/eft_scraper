e46fd30f9ee318f9769cefa7a084be30783207a1
kevin aubel <kevin.aubel@nova-technology.fr>
2021-02-04 18:16:38 +0100

    recherche

---
+++ index.js
@@ -0,0 +1,61 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const marketUrl = "https://tarkov-market.com/";
+    const searchValue = 'san est 10';
+
+    const browser = await puppeteer.launch({
+        headless: true,
+    });
+
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1600,
+        height: 900,
+        deviceScaleFactor: 1,
+      });
+
+    await page.goto(marketUrl, {waitUntil: 'networkidle2'});
+
+    const searchInput = await page.$('input[autofocus="autofocus"]');
+    const bidouille = await page.evaluate( () => document.querySelector('.no-wrap > .price-main').textContent.length);
+
+    await searchInput.type(searchValue);
+    await page.waitForFunction(`document.querySelector('.no-wrap > .price-main').textContent.length !== ${bidouille}`);
+
+    const searchResult = Array();
+    const objectName = await page.$$('.name');
+    const objectPrice = await page.$$('.price-main');
+
+    const traderPrice = await page.$$('.cell .alt');
+
+    const titles = Array();
+    const FleaPrices = Array();
+
+    const traderPrices = Array();
+    const traderNames = Array();
+
+    for (const property in objectName) {
+        titles.push(await page.evaluate(el => el.textContent, objectName[property]))
+    }
+    for (const property in objectPrice) {
+        FleaPrices.push(await page.evaluate(el => el.textContent, objectPrice[property]))
+    }
+    for (const property in traderPrice) {
+        itemPrice = await page.evaluate(el => el.textContent, traderPrice[property])
+        priceWithoutBreak = itemPrice.replace(/(\r\n|\n|\r)/gm, "");
+        priceWithoutSpace = priceWithoutBreak.replace(/\s/g, '');
+        traderPrices.push(priceWithoutSpace);
+    }
+    for (const property in traderPrice) {
+        traderNames.push(await page.evaluate(el => el.nextElementSibling.innerHTML, traderPrice[property]))
+    }
+    for (let i = 0; i < titles.length; i++) {
+        searchResult.push({title: titles[i] , price: FleaPrices[i], trader: traderNames[i], traderPrice: traderPrices[i]})
+    }
+    console.log('result of research for: ' + searchValue);
+    console.table(searchResult);
+   
+    await page.waitForTimeout(1000);
+    await browser.close();
+})();
