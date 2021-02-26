a041a43d27946bb30a96be6db18e9be7070d8952
kevin aubel <kevin.aubel@nova-technology.fr>
2021-02-26 20:12:44 +0100

    compte le nombre d'appel au bot

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ client.on("ready", function () {
 client.on('message', async (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+    // compte le nombre de call du bot
+    require("fs").writeFileSync("config.json",JSON.stringify(require("obop").update({$inc:{call:1}})(JSON.parse(require("fs").readFileSync("config.json")))));
+
     const args = message.content.slice(prefix.length).trim();
     const searchValue = args
 
